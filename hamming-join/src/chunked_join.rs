4a01e07a8914ef9a7c0d4af1739d2df4e507b860
kampersanda <shnsk.knd@gmail.com>
2022-08-31 09:14:59 +0000

    add test

--- hamming-join/src/chunked_join.rs
+++ hamming-join/src/chunked_join.rs
@@ -34,7 +34,7 @@ where
         let mut candidates = vec![];
         for chunk in &self.chunks {
             let r = hamdist / self.num_chunks();
-            let results = MultiSort::similar_pairs(chunk, r, r * 2);
+            let results = MultiSort::similar_pairs(chunk, r, r);
             candidates.extend(results);
         }
         candidates.sort_unstable();
@@ -62,5 +62,61 @@ where
             dist += chunk[i].hamdist(chunk[j]);
         }
         dist
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example_sketches() -> Vec<u16> {
+        vec![
+            0b_1110_0011_1111_1011, // 0
+            0b_0001_0111_0111_1101, // 1
+            0b_1100_1101_1000_1100, // 2
+            0b_1100_1101_0001_0100, // 3
+            0b_1010_1110_0010_1010, // 4
+            0b_0111_1001_0011_1111, // 5
+            0b_1110_0011_0001_0000, // 6
+            0b_1000_0111_1001_0101, // 7
+            0b_1110_1101_1000_1101, // 8
+            0b_0111_1001_0011_1001, // 9
+        ]
+    }
+
+    fn naive_search(sketches: &[u16], radius: f64) -> Vec<(usize, usize, f64)> {
+        let mut results = vec![];
+        for i in 0..sketches.len() {
+            let x = sketches[i];
+            for j in i + 1..sketches.len() {
+                let y = sketches[j];
+                let dist = x.hamdist(y);
+                let dist = dist as f64 / 16.;
+                if dist <= radius {
+                    results.push((i, j, dist));
+                }
+            }
+        }
+        results
+    }
+
+    fn test_similar_pairs(radius: f64) {
+        let sketches = example_sketches();
+        let expected = naive_search(&sketches, radius);
+
+        let mut joiner = ChunkedJoiner::new(2);
+        for s in sketches {
+            joiner.add([(s & 0xFF) as u8, (s >> 8) as u8]);
+        }
+        let mut results = joiner.similar_pairs(radius);
+        results.sort_by_key(|&(i, j, _)| (i, j));
+        assert_eq!(results, expected);
+    }
+
+    #[test]
+    fn test_similar_pairs_for_all() {
+        for radius in 0..=10 {
+            test_similar_pairs(radius as f64 / 10.);
+        }
     }
 }
--- hamming-join/src/simple_join.rs
+++ hamming-join/src/simple_join.rs
@@ -60,3 +60,58 @@ where
         dist
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn example_sketches() -> Vec<u16> {
+        vec![
+            0b_1110_0011_1111_1011, // 0
+            0b_0001_0111_0111_1101, // 1
+            0b_1100_1101_1000_1100, // 2
+            0b_1100_1101_0001_0100, // 3
+            0b_1010_1110_0010_1010, // 4
+            0b_0111_1001_0011_1111, // 5
+            0b_1110_0011_0001_0000, // 6
+            0b_1000_0111_1001_0101, // 7
+            0b_1110_1101_1000_1101, // 8
+            0b_0111_1001_0011_1001, // 9
+        ]
+    }
+
+    fn naive_search(sketches: &[u16], radius: f64) -> Vec<(usize, usize, f64)> {
+        let mut results = vec![];
+        for i in 0..sketches.len() {
+            let x = sketches[i];
+            for j in i + 1..sketches.len() {
+                let y = sketches[j];
+                let dist = x.hamdist(y);
+                let dist = dist as f64 / 16.;
+                if dist <= radius {
+                    results.push((i, j, dist));
+                }
+            }
+        }
+        results
+    }
+
+    fn test_similar_pairs(radius: f64) {
+        let sketches = example_sketches();
+        let expected = naive_search(&sketches, radius);
+
+        let mut joiner = SimpleJoiner::new(2);
+        for s in sketches {
+            joiner.add([(s & 0xFF) as u8, (s >> 8) as u8]);
+        }
+        let results = joiner.similar_pairs(radius);
+        assert_eq!(results, expected);
+    }
+
+    #[test]
+    fn test_similar_pairs_for_all() {
+        for radius in 0..=10 {
+            test_similar_pairs(radius as f64 / 10.);
+        }
+    }
+}
