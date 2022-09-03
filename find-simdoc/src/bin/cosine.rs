8495841bc10d14cd0fea822224bd0e82288495a4
kampersanda <shnsk.knd@gmail.com>
2022-09-03 23:05:36 +0900

    add seeder

--- find-simdoc/src/bin/cosine.rs
+++ find-simdoc/src/bin/cosine.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
@@ -6,6 +7,7 @@ use clap::Parser;
 use find_simdoc::feature::{FeatureConfig, FeatureExtractor};
 use hamming_join::simple_join::SimpleJoiner;
 use lsh::simhash::SimHasher;
+use rand::{RngCore, SeedableRng};
 
 #[derive(Parser, Debug)]
 #[clap(
@@ -27,9 +29,12 @@ struct Args {
 
     #[clap(short = 'c', long, default_value = "8")]
     num_chunks: usize,
+
+    #[clap(short = 's', long)]
+    seed: Option<u64>,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     let text_path = args.text_path;
@@ -37,31 +42,37 @@ fn main() {
     let delimiter = args.delimiter;
     let window_size = args.window_size;
     let num_chunks = args.num_chunks;
+    let seed = args.seed;
 
-    let texts = BufReader::new(File::open(text_path).unwrap())
+    let texts = BufReader::new(File::open(text_path)?)
         .lines()
         .map(|line| line.unwrap());
 
-    let config = FeatureConfig::new(window_size, delimiter, 53);
-    let results = find_in_cosine(texts, radius, num_chunks, config);
+    let mut seeder = rand_xoshiro::SplitMix64::seed_from_u64(seed.unwrap_or(rand::random::<u64>()));
+
+    let config = FeatureConfig::new(window_size, delimiter, seeder.next_u64());
+    let results = find_in_cosine(texts, radius, num_chunks, seeder.next_u64(), config);
 
     println!("i,j,dist");
     for (i, j, dist) in results {
         println!("{i},{j},{dist}");
     }
+
+    Ok(())
 }
 
 fn find_in_cosine<I, S>(
     texts: I,
     radius: f64,
     num_chunks: usize,
+    seed: u64,
     config: FeatureConfig,
 ) -> Vec<(usize, usize, f64)>
 where
     I: Iterator<Item = S>,
     S: AsRef<str>,
 {
-    let hasher = SimHasher::new(42);
+    let hasher = SimHasher::new(seed);
     let mut extractor = FeatureExtractor::new(config);
     let mut joiner = SimpleJoiner::<u64>::new(num_chunks);
 
