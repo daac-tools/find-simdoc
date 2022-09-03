fd78578e72afbe9a9ba6baf6f460623a830c8bf6
kampersanda <shnsk.knd@gmail.com>
2022-09-03 21:27:02 +0900

    split

--- find-simdoc/src/bin/jac.rs
+++ find-simdoc/src/bin/jac.rs
@@ -1,41 +1,17 @@
-pub mod feature;
-pub mod shingling;
-
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
-use std::str::FromStr;
 
 use clap::Parser;
-use feature::{FeatureConfig, FeatureExtractor};
+use find_simdoc::feature::{FeatureConfig, FeatureExtractor};
 use hamming_join::simple_join::SimpleJoiner;
 use lsh::minhash::MinHasher;
-
-#[derive(Clone, Debug)]
-enum Metric {
-    Jaccard,
-    Cosine,
-}
-
-impl FromStr for Metric {
-    type Err = &'static str;
-    fn from_str(metric: &str) -> Result<Self, Self::Err> {
-        match metric {
-            "jac" => Ok(Self::Jaccard),
-            "cos" => Ok(Self::Cosine),
-            _ => Err("Could not parse a metric option"),
-        }
-    }
-}
 
 #[derive(Parser, Debug)]
 #[clap(name = "find-simdoc", about = "A program to find similar documents.")]
 struct Args {
     #[clap(short = 'i', long, action)]
     text_path: PathBuf,
-
-    #[clap(short = 'm', long, action)]
-    metric: Metric,
 
     #[clap(short = 'r', long, action)]
     radius: f64,
@@ -54,7 +30,6 @@ fn main() {
     let args = Args::parse();
 
     let text_path = args.text_path;
-    let _metric = args.metric;
     let radius = args.radius;
     let delimiter = args.delimiter;
     let window_size = args.window_size;
