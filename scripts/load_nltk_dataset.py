015a4b8478979367f4d225b24f6fe797b0ca3abe
kampersanda <shnsk.knd@gmail.com>
2022-09-09 03:38:39 +0900

    add

--- scripts/load_nltk_dataset.py
+++ scripts/load_nltk_dataset.py
@@ -5,14 +5,37 @@ Download NLTK corpora and create a text file of sentences with lowercase letters
 
 import sys
 import nltk
-from nltk.corpus import reuters
 from argparse import ArgumentParser
 
 
 def download_reuters():
+    from nltk.corpus import reuters
     nltk.download('reuters')
-    nltk.download('punkt')
     return reuters.sents()
+
+
+def download_gutenberg():
+    from nltk.corpus import gutenberg
+    nltk.download('gutenberg')
+    return gutenberg.sents()
+
+
+def download_webtext():
+    from nltk.corpus import webtext
+    nltk.download('webtext')
+    return webtext.sents()
+
+
+def download_brown():
+    from nltk.corpus import brown
+    nltk.download('brown')
+    return brown.sents()
+
+
+def download_inaugural():
+    from nltk.corpus import inaugural
+    nltk.download('inaugural')
+    return inaugural.sents()
 
 
 def main():
@@ -20,8 +43,18 @@ def main():
     parser.add_argument('name')
     args = parser.parse_args()
 
+    nltk.download('punkt')
+
     if args.name == 'reuters':
         sents = download_reuters()
+    elif args.name == 'gutenberg':
+        sents = download_gutenberg()
+    elif args.name == 'webtext':
+        sents = download_webtext()
+    elif args.name == 'brown':
+        sents = download_brown()
+    elif args.name == 'inaugural':
+        sents = download_inaugural()
     else:
         print(f'unsupported corpus name: {args.name}', file=sys.stderr)
         return
