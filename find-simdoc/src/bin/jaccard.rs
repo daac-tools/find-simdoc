62fa9f1c64069a163f3a071e126fd7499bbb9936
Shunsuke Kanda <shnsk.knd@gmail.com>
2022-09-08 15:18:33 +0900

    Add more info in progress (#7)
    * add stat
    
    * add progress

--- find-simdoc/src/bin/jaccard.rs
+++ find-simdoc/src/bin/jaccard.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
+use std::time::Instant;
 
 use clap::Parser;
 use find_simdoc::feature::{FeatureConfig, FeatureExtractor};
@@ -53,7 +54,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let num_chunks = args.num_chunks;
     let seed = args.seed;
 
-    assert_ne!(window_size, 0);
+    if window_size == 0 {
+        return Err("window_size must not be 0.".into());
+    }
 
     let texts = BufReader::new(File::open(document_path)?)
         .lines()
@@ -90,29 +93,38 @@ where
     let mut extractor = FeatureExtractor::new(config);
     let mut joiner = ChunkedJoiner::<u64>::new(num_chunks).shows_progress(true);
 
-    eprintln!("Converting sentences into sketches...");
-    let mut feature = vec![];
-    for (i, text) in texts.enumerate() {
-        if (i + 1) % 1000 == 0 {
-            eprintln!("Processed {} sentences...", i + 1);
+    // TODO: Multi-threading.
+    {
+        eprintln!("Converting sentences into sketches...");
+        let start = Instant::now();
+        let mut feature = vec![];
+        for (i, text) in texts.enumerate() {
+            if (i + 1) % 1000 == 0 {
+                eprintln!("Processed {} sentences...", i + 1);
+            }
+            let text = text.as_ref();
+            assert!(!text.is_empty());
+            extractor.extract(text, &mut feature);
+            joiner.add(hasher.iter(&feature))?;
         }
-        let text = text.as_ref();
-        assert!(!text.is_empty());
-        extractor.extract(text, &mut feature);
-        joiner.add(hasher.iter(&feature))?;
+        let duration = start.elapsed();
+        let memory_in_bytes = joiner.memory_in_bytes() as f64;
+        eprintln!(
+            "Produced {} sketches in {} sec, consuming {} MiB",
+            joiner.num_sketches(),
+            duration.as_secs_f64(),
+            memory_in_bytes / (1024. * 1024.)
+        );
     }
-    let memory_in_bytes = joiner.memory_in_bytes() as f64;
-    eprintln!(
-        "Produced {} sketches in {} MiB",
-        joiner.num_sketches(),
-        memory_in_bytes / (1024. * 1024.)
-    );
 
+    eprintln!("Finding all similar pairs in sketches...");
+    let start = Instant::now();
     // In 1-bit minhash, the collision probability is multiplied by 2 over the original.
     // Thus, we should search with the half of the actual radius.
     let mut results = joiner.similar_pairs(radius / 2.);
-
     // Modifies the distances.
     results.iter_mut().for_each(|(_, _, d)| *d *= 2.);
+    eprintln!("Done in {} sec", start.elapsed().as_secs_f64());
+
     Ok(results)
 }
