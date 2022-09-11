72f7db6cc761a6331ab7743db33f2fded1433e39
kampersanda <shnsk.knd@gmail.com>
2022-09-11 15:27:20 +0900

    add example

--- find-simdoc/src/cosine.rs
+++ find-simdoc/src/cosine.rs
@@ -17,6 +17,45 @@ use rand::{RngCore, SeedableRng};
 ///    where a feature is a tfidf-weighted vector representation of character or word ngrams.
 /// 2. Convert the features into binary sketches through the [simplified simhash](https://dl.acm.org/doi/10.1145/1242572.1242592).
 /// 3. Search for similar sketches in the Hamming space using [`ChunkedJoiner`].
+///
+/// # Examples
+///
+/// ```
+/// use find_simdoc::tfidf::{Idf, Tf};
+/// use find_simdoc::CosineSearcher;
+///
+/// fn main() {
+///     let documents = vec![
+///         "Welcome to Jimbocho, the town of books and curry!",
+///         "Welcome to Jimbocho, the city of books and curry!",
+///         "We welcome you to Jimbocho, the town of books and curry.",
+///         "Welcome to the town of books and curry, Jimbocho!",
+///     ];
+///
+///     // Creates a searcher for word unigrams (with random seed value 42).
+///     let searcher = CosineSearcher::new(1, Some(' '), Some(42)).unwrap();
+///     // Creates a term frequency (TF) weighter.
+///     let tf = Tf::new();
+///     // Creates a inverse document frequency (IDF) weighter.
+///     let idf = Idf::new()
+///         .build(documents.iter().clone(), searcher.config())
+///         .unwrap();
+///     // Builds the database of binary sketches converted from input documents,
+///     let searcher = searcher
+///         // with the TF weighter and
+///         .tf(Some(tf))
+///         // the IDF weighter,
+///         .idf(Some(idf))
+///         // where binary sketches are in the Hamming space of 10*64 dimensions.
+///         .build_sketches(documents, 10)
+///         .unwrap();
+///
+///     // Searches all similar pairs within radius 0.25.
+///     let results = searcher.search_similar_pairs(0.25);
+///     // A result consists of the left-side id, the right-side id, and their distance.
+///     assert_eq!(results, vec![(0, 1, 0.1671875), (0, 3, 0.246875)]);
+/// }
+/// ```
 pub struct CosineSearcher {
     config: FeatureConfig,
     hasher: SimHasher,
--- find-simdoc/src/jaccard.rs
+++ find-simdoc/src/jaccard.rs
@@ -16,6 +16,33 @@ use rand::{RngCore, SeedableRng};
 ///    where a feature is a set representation of character or word ngrams.
 /// 2. Convert the features into binary sketches through the [1-bit minwise hashing](https://arxiv.org/abs/0910.3349).
 /// 3. Search for similar sketches in the Hamming space using [`ChunkedJoiner`].
+///
+/// # Examples
+///
+/// ```
+/// use find_simdoc::JaccardSearcher;
+///
+/// fn main() {
+///     let documents = vec![
+///         "Welcome to Jimbocho, the town of books and curry!",
+///         "Welcome to Jimbocho, the city of books and curry!",
+///         "We welcome you to Jimbocho, the town of books and curry.",
+///         "Welcome to the town of books and curry, Jimbocho!",
+///     ];
+///
+///     // Creates a searcher for character trigrams (with random seed value 42).
+///     let searcher = JaccardSearcher::new(3, None, Some(42))
+///         .unwrap()
+///         // Builds the database of binary sketches converted from input documents,
+///         // where binary sketches are in the Hamming space of 20*64 dimensions.
+///         .build_sketches(documents, 20)
+///         .unwrap();
+///
+///     // Searches all similar pairs within radius 0.25.
+///     let results = searcher.search_similar_pairs(0.25);
+///     assert_eq!(results, vec![(0, 1, 0.1875), (0, 3, 0.2296875)]);
+/// }
+/// ```
 pub struct JaccardSearcher {
     config: FeatureConfig,
     hasher: MinHasher,
