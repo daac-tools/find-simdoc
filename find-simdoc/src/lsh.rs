9ca71a42ee41c81c6d8ac1d5fd47279a3e50533d
kampersanda <shnsk.knd@gmail.com>
2022-09-16 23:11:54 +0900

    add doc

--- find-simdoc/src/lsh.rs
+++ find-simdoc/src/lsh.rs
@@ -14,6 +14,16 @@ pub(crate) fn hash_u64(x: u64, seed: u64) -> u64 {
 }
 
 /// Computes the Jaccard distance.
+///
+/// # Examples
+///
+/// ```
+/// use find_simdoc::lsh::jaccard_distance;
+///
+/// let x = vec![1, 2, 4];
+/// let y = vec![1, 2, 5, 7];
+/// assert_eq!(jaccard_distance(x, y), 0.6);
+/// ```
 pub fn jaccard_distance<I, T>(lhs: I, rhs: I) -> f64
 where
     I: IntoIterator<Item = T>,
